06367cdb20363a60bdb5df43f525e51a030e6dc2
harshit9715 <harshit9715@gmail.com>
2021-04-04 14:52:15 +0530

    basic next app with routes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import { getSortedPostsData } from '../lib/posts'
-
 import Head from 'next/head'
+import Link from 'next/link'
+
+import { getSortedPostsData } from '../lib/posts'
+import Date from '../components/date'
 import Layout, { siteTitle, name } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -20,19 +23,21 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <b>{name}</b>. I'm a software engineer and full stack web developer. You can contact me on{' '}
-          <a href='https://www.linkedin.com/in/harshit9715/'>Linked<span>In</span></a></p>
+        <p>Hello, I'm <b>{name}</b>. I'm a software engineer and full stack web developer from Hyderabad, India. You can contact me on{' '}
+          <a href='https://www.linkedin.com/in/harshit9715/'><span>LinkedIn</span></a></p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
