cdb3e3fcc2e99cc48156c29eaae8b687b2044181
harshit9715 <harshit9715@gmail.com>
2021-04-18 20:32:06 +0530

    checkpoint

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,8 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
