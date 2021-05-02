5b95ce1aaaebd68690ea0996500bdb70f1af6aa5
harshit9715 <harshit9715@gmail.com>
2021-05-02 21:41:05 +0530

    updated worl-life-timeline

--- pages/index.js
+++ pages/index.js
@@ -27,9 +27,9 @@ export default function Home({ allPostsData }) {
           <a href='https://www.linkedin.com/in/harshit9715/'><span>LinkedIn</span></a></p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Work-life so far...</h2>
-          <p>[W.I.P] You can check it <Link href={`/work-life`}><a>here</a></Link>.</p>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Work-life timeline...</h2>
+          <p>You can check it <Link href={`/work-life`}><a>here</a></Link>.</p>
+        <h2 className={utilStyles.headingLg}>Blogs</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
--- pages/work-life/index.js
+++ pages/work-life/index.js
@@ -6,8 +6,9 @@ import 'react-vertical-timeline-component/style.min.css';
 import Layout, { siteTitle, name } from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
 import workData from '../../public/assets/workData.json';
+import { useState } from 'react';
 export default function WorkLife() {
-
+    const [showDetail, setShowDetail] = useState(false);
     const colors = (op = 1) => [`rgba(16, 204, 82, ${op})`, `rgba(233, 30, 99, ${op})`, `rgba(33, 150, 243, ${op})`]
     return (
         <Layout>
@@ -19,47 +20,42 @@ export default function WorkLife() {
                     <a href='https://www.linkedin.com/in/harshit9715/'><span>LinkedIn</span></a></p>
             </section>
             {/* <Scrollbars style={{ height: 600 }}> */}
-                <VerticalTimeline>
-                    {workData.map((item, index) => (
-                        <VerticalTimelineElement
-                            key={index}
-                            className="vertical-timeline-element--work"
-                            dateClassName={utilStyles.timeline_date_fix}
-                            contentStyle={{ background: colors()[index % 3] }}
-                            contentArrowStyle={{ borderRight: `7px solid  ${colors()[index % 3]}` }}
-                            date={item.date}
-                            iconStyle={{ background: colors()[index % 3], color: '#fff' }}
-                            icon={<div />}
-                        >
-                            <h3 className="vertical-timeline-element-title">{item.title}</h3>
-                            <h4 className="vertical-timeline-element-subtitle">{item.subTitle}</h4>
-                            {
-                                ((item.description.length === 1) ?
-                                    (<p>{item.description}</p>) :
-                                    (<ul>
-                                        {item.description.map((l_item, ix) => (<li key={ix}>{l_item}</li>))}
-                                    </ul>))
-                            }
-                            {
-                                // item.refs.map((ref, ix) => (<a key={ix} href={ref.url}>{ref.name}</a>))
-                                item.refs.map((ref, ix) => (
-                                    <button
-                                        onClick={() => { window.open(ref.url, "_blank") }}
-                                        className={utilStyles.pushable}>
-                                        <span className={utilStyles.shadow}></span>
-                                        <span className={utilStyles.edge} style={{ background: colors(0.1)[index % 3] }}></span>
-                                        <span className={utilStyles.front} style={{ background: colors(0.7)[index % 3] }}>{ref.name}</span>
-                                    </button>
-
-                                ))
-                            }
-                        </VerticalTimelineElement>
-                    ))}
+            <label>Detailed? </label>
+            <label className={utilStyles.life__detail_switch}>
+                <input type="checkbox" onClick={() => { setShowDetail(!showDetail) }} />
+                <span className={utilStyles.life__slider_round}></span>
+            </label>
+            <VerticalTimeline>
+                {workData.map((item, index) => (
                     <VerticalTimelineElement
-                        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                        key={index}
+                        // className="vertical-timeline-element--work"
+                        dateClassName={utilStyles.timeline_date_fix}
+                        contentStyle={{ background: colors()[index % 3], marginLeft: '0' }}
+                        contentArrowStyle={{ borderRight: `7px solid  ${colors()[index % 3]}` }}
+                        date={item.date}
+                        iconStyle={{ background: colors()[index % 3], color: '#fff' }}
                         icon={<div />}
-                    />
-                </VerticalTimeline>
+                    >
+                        <h3 className="vertical-timeline-element-title">{item.title}</h3>
+                        <h4 className="vertical-timeline-element-subtitle">{item.subTitle}</h4>
+                        { showDetail && (<p>{item.description}</p>)}
+                        {
+                            // item.refs.map((ref, ix) => (<a key={ix} href={ref.url}>{ref.name}</a>))
+                            item.refs.map((ref, ix) => (
+                                <button
+                                    key={ix}
+                                    onClick={() => { window.open(ref.url, ref.url.startsWith('/') ? "_self" : "_blank") }}
+                                    className={utilStyles.pushable}>
+                                    <span className={utilStyles.shadow}></span>
+                                    <span className={utilStyles.edge} style={{ background: colors(0.1)[index % 3] }}></span>
+                                    <span className={utilStyles.front} style={{ background: colors(0.7)[index % 3] }}>{ref.name}</span>
+                                </button>
+                            ))
+                        }
+                    </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
             {/* </Scrollbars> */}
         </Layout>
     )
