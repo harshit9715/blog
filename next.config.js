b63c8b4eaaa51600e2b5f1695e9fe4be08fd2c89
harshit9715 <harshit9715@gmail.com>
2021-09-26 02:26:14 +0530

    updated next and react

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const optimizedImages = require('next-optimized-images');
 
 module.exports = withPlugins([
   [optimizedImages, {
-    future: {
-      webpack5: true,
-    },
     // these are the default values so you don't have to provide them if they are good enough for your use-case.
     // but you can overwrite them here with any valid value you want.
     inlineImageLimit: 8192,
+    images: {
+      disableStaticImages: true
+    },
     imagesFolder: 'images',
     imagesName: '[name]-[hash].[ext]',
     handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
